2059f2d5c202c43f68187248c521f338be8a8390
MaheshGupta <harryinandroid@gmail.com>
2017-07-19 13:56:36 +0000

    Started network connection

--- app/src/main/java/com/tutorials/andorid/app/HomeActivity.java
+++ app/src/main/java/com/tutorials/andorid/app/HomeActivity.java
@@ -17,6 +17,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tutorials.andorid.app.model.UserProfile;
+import com.tutorials.andorid.app.model.user.User;
+import com.tutorials.andorid.app.service.NetworkConnection;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -27,6 +32,7 @@ public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
 
     private int fragmentIndex = 0;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +47,12 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, TAG + "onResume: ");
+        setStatus("Click any button");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        setStatus("Loading...");
     }
 
 
@@ -93,11 +98,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void showProgress(View view) {
-        final ProgressDialog progressDialog = new ProgressDialog(HomeActivity.this);
 
-        progressDialog.setTitle("Downloading");
-        progressDialog.setMessage("Please wait while downloading");
-        progressDialog.show();
 
         new Thread(new Runnable() {
             @Override
@@ -117,7 +118,7 @@ public class HomeActivity extends AppCompatActivity {
         builder.setTitle("Question");
         builder.setMessage("Do you want to proceed ?");
         builder.setCancelable(false);
-        
+
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -130,11 +131,39 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
-                Toast.makeText(HomeActivity.this, "Cancel Clicked", Toast.LENGTH_SHORT).show();            }
+                Toast.makeText(HomeActivity.this, "Cancel Clicked", Toast.LENGTH_SHORT).show();
+            }
         });
 
 
         builder.show();
+    }
+
+    public void connectToNetwork(View view) {
+        try {
+            showProgress("", "Please Wait...");
+            NetworkConnection<ArrayList<User>> connection = new NetworkConnection("https://jsonplaceholder.typicode.com/users");
+            connection.makeRequest(NetworkConnection.RequestType.GET, new NetworkConnection.Callback() {
+                @Override
+                public void onSuccess(Object response) {
+                    dismissProgress();
+                    Log.i(TAG, "onSuccess: ");
+                    ArrayList<User> users = (ArrayList<User>) response;
+                    TextView txtViewStatus = (TextView) findViewById(R.id.txt_view_status);
+                    txtViewStatus.setText("Success response");
+//                    setStatus("Number of Users : " + users.size());
+                }
+
+                @Override
+                public void onError(String error) {
+                    dismissProgress();
+                    Log.i(TAG, "onError: ");
+                    setStatus("Some error");
+                }
+            });
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -151,4 +180,26 @@ public class HomeActivity extends AppCompatActivity {
             imageView.setImageBitmap(bitmap);
         }
     }
+
+    private void showProgress(String title, String message) {
+        if (this.progressDialog == null) {
+            this.progressDialog = new ProgressDialog(HomeActivity.this);
+        }
+
+        if (!this.progressDialog.isShowing()) {
+            this.progressDialog.dismiss();
+        }
+
+        progressDialog.setTitle(title);
+        progressDialog.setMessage(message);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
+    private void dismissProgress() {
+        if (this.progressDialog != null && this.progressDialog.isShowing()) {
+            this.progressDialog.dismiss();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/tutorials/andorid/app/model/user/Geo.java
@@ -0,0 +1,23 @@
+package com.tutorials.andorid.app.model.user;
+
+public class Geo {
+    private String lat;
+
+    public String getLat() {
+        return this.lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    private String lng;
+
+    public String getLng() {
+        return this.lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}
